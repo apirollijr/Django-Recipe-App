81095dc1e13816dc2949b9a421a169506c177d84
Anthony Pirolli Jr <apirollijr@gmail.com>
2026-01-12 06:39:38 -0500

    Fix LOGIN_URL setting

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -127,6 +127,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authentication settings
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
